e8edbb948032e1125b3eba977daf4d0f843f74db
bmihaljevic <boki.mihaljevic@gmail.com>
2017-09-28 17:57:33 +0200

    initial

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+load the data set
+combine the classifiers
+try with more data. that would require neurostr and my code on more cells.
+if you combine the classifiers, you may good results. just apply some useful ones.
+
+
+// I can import the classifiers and models directly from source files.
+// Code:
+    learning and models. get this by source, probably will not be reused
+    mlr: this is OK to be exported elsewhere, e.g., to mlearn. can be tested as well
